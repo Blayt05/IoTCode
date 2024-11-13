7925aef99a276c9eeb7cc97dff23328e38c798ba
Israel Booz <israelbooz05@gmail.com>
2024-11-13 16:08:39 -0600

    Avance 6 microcontroladores

--- ArduinoCodes/config.h
+++ ArduinoCodes/config.h
@@ -1,8 +1,8 @@
 const char* ssid     = "Tec-IoT";
 const char* password = "spotless.magnetic.bridge";
 
-const char* hostname = "ESP8266_4";
+const char* hostname = "ESP8266_6";
 
-IPAddress ip(10, 22, 187, 21);
+IPAddress ip(10, 22, 191, 55);
 IPAddress gateway(10, 22, 176, 1);
 IPAddress subnet(255, 255, 240, 0);
--- PythonCode/semaforo1.py
+++ PythonCode/semaforo1.py
@@ -30,8 +30,14 @@ type_light_client2 = "Semaforo Peatonal"
 coordinates_client3 = "26.777777, -101.777777"
 type_light_client3 = "Semaforo Vehicular"
 
-coordinates_client4 = "28.888888, -102.888888"
+coordinates_client4 = "27.888888, -102.888888"
 type_light_client4 = "Semaforo Peatonal"
+
+coordinates_client5 = "29.999999, -103.999999"
+type_light_client5 = "Semaforo Vehicular"
+
+coordinates_client6 = "30.000000, -104.000000"
+type_light_client6 = "Semaforo Peatonal"
 
 # Setup the MySQL database and table
 def setup_database():
@@ -58,11 +64,15 @@ client1 = paho.Client()
 client2 = paho.Client()
 client3 = paho.Client()
 client4 = paho.Client()
+client5 = paho.Client()
+client6 = paho.Client()
 
 data1 = None
 data2 = None
 data3 = None
 data4 = None
+data5 = None
+data6 = None
 data_color_time = None
 
 apprun = True
@@ -186,6 +196,16 @@ def message_handling_4(client, userdata, msg):
     data4 = int(msg.payload.decode())
     insert_data(coordinates_client4, type_light_client4, data4, 'Rojo', 3)
 
+def message_handling_5(client, userdata, msg):
+    global data5
+    data5 = int(msg.payload.decode())
+    insert_data(coordinates_client5, type_light_client5, data5, 'Verde', 5)
+
+def message_handling_6(client, userdata, msg):
+    global data6
+    data6 = int(msg.payload.decode())
+    insert_data(coordinates_client6, type_light_client6, data6, 'Rojo', 3)
+
 def message_handling_color_time(client, userdata, msg):
     try:
         message = msg.payload.decode()
@@ -200,8 +220,12 @@ def message_handling_color_time(client, userdata, msg):
             insert_data(coordinates_client2, type_light_client2, data2, color, time_leds)
         elif client == client3:
             insert_data(coordinates_client3, type_light_client3, data3, color, time_leds)
-        else:
+        elif client == client4:
             insert_data(coordinates_client4, type_light_client4, data4, color, time_leds)
+        elif client == client5:
+            insert_data(coordinates_client5, type_light_client5, data5, color, time_leds)
+        else:
+            insert_data(coordinates_client6, type_light_client6, data6, color, time_leds)
 
         print(f"Color: {color}, Time: {time_leds}")
 
@@ -224,10 +248,21 @@ def loop_4(num):
     global client4
     client4.loop_forever()
 
+def loop_5(num):
+    global client5
+    client5.loop_forever()
+
+def loop_6(num):
+    global client6
+    client6.loop_forever()
+
 client1.on_message = message_handling_1
 client2.on_message = message_handling_2
 client3.on_message = message_handling_3
 client4.on_message = message_handling_4
+client5.on_message = message_handling_5
+client6.on_message = message_handling_6
+
 
 client1.message_callback_add("arduino_1/led_state", message_handling_color_time)
 client1.message_callback_add("arduino_1/sensor_deteccion", message_handling_1)
@@ -241,34 +276,52 @@ client3.message_callback_add("arduino_3/sensor_deteccion", message_handling_3)
 client4.message_callback_add("arduino_4/led_state", message_handling_color_time)
 client4.message_callback_add("arduino_4/sensor_deteccion", message_handling_4)
 
+client5.message_callback_add("arduino_5/led_state", message_handling_color_time)
+client5.message_callback_add("arduino_5/sensor_deteccion", message_handling_5)
+
+client6.message_callback_add("arduino_6/led_state", message_handling_color_time)
+client6.message_callback_add("arduino_6/sensor_deteccion", message_handling_6)
+
 def signal_handler(sig, frame):
     global client1
     global client2
     global client3
     global client4
+    global client5
+    global client6
     print('You pressed Ctrl+C!')
     client1.disconnect()
     client2.disconnect()
     client3.disconnect()
     client4.disconnect()
+    client5.disconnect()
+    client6.disconnect()
     print("Quit")
     exit(0)
     
 signal.signal(signal.SIGINT, signal_handler)
 
-if client1.connect("10.22.187.21", 1883, 60) != 0:
+if client1.connect("10.22.191.55", 1883, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     exit(1)
 
-if client2.connect("10.22.187.21", 1883, 60) != 0:
+if client2.connect("10.22.191.55", 1883, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     exit(1)
 
-if client3.connect("10.22.187.21", 1883, 60) != 0:
+if client3.connect("10.22.191.55", 1883, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     exit(1)
 
-if client4.connect("10.22.187.21", 1883, 60) != 0:
+if client4.connect("10.22.191.55", 1883, 60) != 0:
+    print("Couldn't connect to the mqtt broker")
+    exit(1)
+
+if client5.connect("10.22.191.55", 1883, 60) != 0:
+    print("Couldn't connect to the mqtt broker")
+    exit(1)
+
+if client6.connect("10.22.191.55", 1883, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     exit(1)
 
@@ -280,6 +333,11 @@ client3.subscribe("arduino_3/sensor_deteccion")
 client3.subscribe("arduino_3/led_state")
 client4.subscribe("arduino_4/sensor_deteccion")
 client4.subscribe("arduino_4/led_state")
+client5.subscribe("arduino_5/sensor_deteccion")
+client5.subscribe("arduino_5/led_state")
+client6.subscribe("arduino_6/sensor_deteccion")
+client6.subscribe("arduino_6/led_state")
+
 
 try:
     print("Press CTRL+C to exit...")
@@ -287,11 +345,15 @@ try:
     t2 = threading.Thread(target=loop_2, args=(0,))
     t3 = threading.Thread(target=loop_3, args=(0,))
     t4 = threading.Thread(target=loop_4, args=(0,))
+    t5 = threading.Thread(target=loop_5, args=(0,))
+    t6 = threading.Thread(target=loop_6, args=(0,))
     
     t1.start()
     t2.start()
     t3.start()
     t4.start()
+    t5.start()
+    t6.start()
     while apprun:
         try:
             time.sleep(0.5)
@@ -299,6 +361,8 @@ try:
             print("data2:", data2)
             print("data3:", data3)
             print("data4:", data4)
+            print("data5:", data5)
+            print("data6:", data6)
             print("----")
         except KeyboardInterrupt:
             print("Disconnecting")
@@ -307,12 +371,16 @@ try:
             client2.disconnect()
             client3.disconnect()
             client4.disconnect()
+            client5.disconnect()
+            client6.disconnect()
             time.sleep(1)
     
     t1.join()
     t2.join()
     t3.join()
     t4.join()
+    t5.join()
+    t6.join()
     
 except Exception:
     print("Caught an Exception, something went wrong...")
